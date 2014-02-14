d3a765cea5a6d613f898344a218b0570a8c87bd2
Jack Chen <s810434@gmail.com>
2014-02-14 15:27:38 -0800

    trivial

--- sprintron_keyfob_app.c
+++ sprintron_keyfob_app.c
@@ -777,9 +777,9 @@ static void peripheralStateNotificationCB( gaprole_States_t newState )
         // if beep status is on, turn it off, and stop alert.
         if( keyfobBeepStatus != BEEP_STATUS_NONE )
         {
-            keyfobBeepStatus = BEEP_STATUS_NONE;
-            SprintronKeyfob_SetParameter( SPRINTRON_KEYFOB_BEEP_STATUS,  sizeof ( uint8 ), &keyfobBeepStatus );
-            keyfobapp_StopAlert();
+          keyfobBeepStatus = BEEP_STATUS_NONE;
+          SprintronKeyfob_SetParameter( SPRINTRON_KEYFOB_BEEP_STATUS,  sizeof ( uint8 ), &keyfobBeepStatus );
+          keyfobapp_StopAlert();
         }
 
         // Turn off LED that shows we're advertising
@@ -794,9 +794,9 @@ static void peripheralStateNotificationCB( gaprole_States_t newState )
         // if beep status is on, turn it off, and stop alert.
         if( keyfobBeepStatus != BEEP_STATUS_NONE )
         {
-            keyfobBeepStatus = BEEP_STATUS_NONE;
-            SprintronKeyfob_SetParameter( SPRINTRON_KEYFOB_BEEP_STATUS,  sizeof ( uint8 ), &keyfobBeepStatus );
-            keyfobapp_StopAlert();
+          keyfobBeepStatus = BEEP_STATUS_NONE;
+          SprintronKeyfob_SetParameter( SPRINTRON_KEYFOB_BEEP_STATUS,  sizeof ( uint8 ), &keyfobBeepStatus );
+          keyfobapp_StopAlert();
         }
       }
       break;
